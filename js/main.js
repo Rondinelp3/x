3253d90387e2fce3388516b3edc4a68b4eecba10
Rondinelp3 <daniel_rondinelp3@hotmail.com>
2020-02-25 21:32:01 -0500

    casi

---
+++ js/main.js
@@ -0,0 +1,24 @@
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
+   $(document).ready(function () {
+        $('.sidenav').sidenav();
+    });
+    $(document).ready(function () {
+        $('.slider').slider({
+
+            height : 1000, // default - height : 400
+            interval: 8000 // default - interval: 6000
+        });
+    });
+
+ $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+        
+ 
+/*lazyload*/
+  $(function() {
+        $('.lazy').Lazy();
+    });
+           
